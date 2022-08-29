68bb831112db985522a245d78a4a6ff21953aed5
Jaksa Jokunen <testinumero123@gmail.com>
2022-08-29 13:55:05 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 275;
+
+        boolean sama = false;
+
+        if(a % 10 == b % 10)
+        {
+            sama = true;
+        }
+        System.out.println(sama);
+    }
+}
